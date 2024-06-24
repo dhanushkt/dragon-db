372788bb4ebf75404fe70b256b3bcb08b73d460f
dhanushkt <dhanushkt787@gmail.com>
2024-06-24 19:15:08 +0530

    MKV Rename API with script

---
+++ api/mkvrename_api.php
@@ -0,0 +1,164 @@
+<?php
+/* 
+This PHP API code is designed to automatically execute scripts using the ssh2 extension on a remote server where the media is located, to perform mkvrename operations. Script file to be executed (.sh) should be placed on VM.
+Triggered by Radarr/Sonarr WebHook Connection for "Import" or "Upgrade" events.
+API by: Dragon DB 
+*/
+
+// API Flags
+$skip_anime = true;
+
+// Create a log file to store the API logs
+date_default_timezone_set('Asia/Kolkata');
+$log_dir = "mkvrename_api_logs";
+// Check if the directory exists, and create it if it doesn't
+if (!is_dir($log_dir)) {
+    mkdir($log_dir, 0777, true);
+}
+$log_current_date = date('d-m-Y_H-i-s');
+$log_filename = $log_dir . "/mkvrename_api_log_$log_current_date.txt";
+$log_file = fopen($log_filename, 'a');
+
+/* Functions */
+// Function to remove escaping backslashes
+function clean_path($path)
+{
+    return str_replace('\\', '', $path);
+}
+
+// Function to execute the remote script using ssh2
+function executeRemoteScript($host, $port, $username, $password, $scriptPath, $params)
+{
+    // Establish a connection to the remote host
+    $connection = ssh2_connect($host, $port);
+    if (!$connection) {
+        return ('SSH Connection failed');
+        //die('Connection failed');
+    }
+
+    // Authenticate with the remote host
+    if (!ssh2_auth_password($connection, $username, $password)) {
+        return ('SSH Authentication failed');
+        //die('Authentication failed');
+    }
+
+    // Create the command to execute
+    $command = $scriptPath . ' ' . implode(' ', array_map('escapeshellarg', $params));
+
+    // Execute the command
+    $stream = ssh2_exec($connection, $command);
+    if (!$stream) {
+        return ('SSH Command execution failed');
+        //die('Command execution failed');
+    }
+
+    // Set stream blocking and fetch the output
+    stream_set_blocking($stream, true);
+    $output = stream_get_contents($stream);
+
+    // Close the stream
+    fclose($stream);
+
+    // Return the output
+    return "Sending command: \n" . $command . "\n" . "Output from Remote Script: \n" . $output;
+}
+
+// Function to check if path containes "Anime"
+function containsAnime($path){
+    $searchPath = '/media/Anime';
+    return strpos($path, $searchPath) !== false;
+}
+
+fwrite($log_file, "====================================================== \n");
+fwrite($log_file, "Triggered MKV Rename API on " . date('d-m-Y h:i:s A') . "\n");
+fwrite($log_file, "====================================================== \n");
+
+
+// Get the header "User Agent" from $_SERVER
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    fwrite($log_file, "User-Agent: " . $userAgent . "\n");
+} else {
+    fwrite($log_file, "User-Agent header is not set.\n");
+}
+
+// Get the body content from API response
+$api_body = file_get_contents('php://input');
+$api_body_json = json_decode($api_body, true);
+fwrite($log_file, "API Body Content: " . $api_body . "\n");
+
+// There are two possibility in api_body_json - movie or series, check and select values accordingly
+$folderPath = '';
+$relativePath = '';
+$clean_folderPath = '';
+$clean_relativePath = '';
+
+// Check if the data structure is for a movie
+if (isset($api_body_json['movie']) && isset($api_body_json['movieFile'])) {
+    $folderPath = $api_body_json['movie']['folderPath'];
+    $clean_folderPath = clean_path($folderPath);
+    $relativePath = $api_body_json['movieFile']['relativePath'];
+    $clean_relativePath = clean_path($relativePath);
+
+    // log
+    fwrite($log_file, "Radarr Folder Path: " . $folderPath . "\n");
+    fwrite($log_file, "Radarr Clean Folder Path: " . $clean_folderPath . "\n");
+    fwrite($log_file, "Radarr Relative Path: " . $relativePath . "\n");
+    fwrite($log_file, "Radarr Clean Relative Path: " . $clean_relativePath . "\n");
+}
+
+// Check if the data structure is for a series
+elseif (isset($api_body_json['series']) && isset($api_body_json['episodeFile'])) {
+    $folderPath = $api_body_json['series']['path'];
+    $clean_folderPath = clean_path($folderPath);
+    $relativePath = $api_body_json['episodeFile']['relativePath'];
+    $clean_relativePath = clean_path($relativePath);
+
+    // log
+    fwrite($log_file, "Sonarr Folder Path: " . $folderPath . "\n");
+    fwrite($log_file, "Sonarr Clean Folder Path: " . $clean_folderPath . "\n");
+    fwrite($log_file, "Sonarr elative Path: " . $relativePath . "\n");
+    fwrite($log_file, "Sonarr Clean Relative Path: " . $clean_relativePath . "\n");
+}
+
+// Check for Anime in folderPath and skip it if skip_anime is true
+if ($skip_anime && containsAnime($clean_folderPath)) {
+    fwrite($log_file, "Skip Anime is enabled. Exiting \n");
+    fwrite($log_file, "====================================================== \n");
+    fclose($log_file);
+    http_response_code(200);
+    echo json_encode([
+        "status" => "error",
+        "message" => "Anime is Skipped."
+    ]);
+    exit(0);
+}
+
+// Check if clean_folderPath and clean_relativePath are not empty and if it is empty then exit the php code
+if (empty($clean_folderPath) || empty($clean_relativePath)) {
+    fwrite($log_file, "Folder Path or Relative Path is empty. Exiting \n");
+    fwrite($log_file, "====================================================== \n");
+    fclose($log_file);
+
+    // Send HTTP 200 OK response before exiting
+    http_response_code(200);
+    echo json_encode([
+        "status" => "error",
+        "message" => "Folder Path or Relative Path is empty."
+    ]);
+    exit(0);
+}
+
+// Combine clean folder path and relative path
+$final_path = $clean_folderPath . '/' . $clean_relativePath;
+fwrite($log_file, "Final Path: " . $final_path . "\n");
+
+// Send an SSH command to the remote server
+include_once ('mkvrename_creds.php');
+$ssh_output = executeRemoteScript($creds_host, $creds_port, $creds_username, $creds_password, $creds_scriptPath, [$final_path]);
+fwrite($log_file, "SSH Function Output: \n" . $ssh_output . "\n");
+
+fwrite($log_file, "End of MKV Rename API \n");
+fwrite($log_file, "====================================================== \n");
+
+fclose($log_file);
---
+++ api/mkvrename_api.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+# Script to automatically remove unwanted names from MKV track metadata after Radarr/Sonarr import or upgrade
+# PHP API is implamented to trigger this script remotely | MKVRN
+# Requirements: mkvinfo, mkvpropedit
+# Author: Dragon DB
+
+# Global Log Variables
+mkvlog_current_datetime=$(date +"%d-%m-%Y_%H-%M-%S")
+mkvlog_output_file="/home/trinityvoid/scripts/mkvrename_api_logs/mkvrename_api_sh_log_${mkvlog_current_datetime}.txt"
+# Log flag to log mkvinfo & mediainfo if there are any changes
+mkv_log=true
+
+mkv_rename_func() {
+    # Full file path of .mkv file (argument)
+    local filename=$1
+    # Word to be searched in Track name (argument)
+    local word_to_remove=$2
+    
+    echo "=================================================================" >> "$mkvlog_output_file"
+    echo "> MKV Rename Function | -----------------------------------------" >> "$mkvlog_output_file"
+    echo "" >> "$mkvlog_output_file"
+    echo "> File Name | ---------------------------------------------------" >> "$mkvlog_output_file"
+    echo "$filename" >> "$mkvlog_output_file"
+    echo "> Word to remove | ----------------------------------------------" >> "$mkvlog_output_file"
+    echo "$word_to_remove" >> "$mkvlog_output_file"
+    
+    # Loop through tracks and delete the name field if it contains the "word_to_remove"
+    mkvinfo "$filename" | grep "Track number" | while read -r line; do
+        track_number=$(echo "$line" | awk '{print $5}')
+        
+        # echo "#################################################################" >> "$mkvlog_output_file"
+        echo "_________________________________________________________________" >> "$mkvlog_output_file"
+        echo "> | TRACK NUMBER: $track_number |" >> "$mkvlog_output_file"
+        
+        current_name=$(mkvinfo "$filename" | grep -A 10 "Track number: $track_number" | grep "Name" | sed 's/^[[:space:]]*Name:[[:space:]]*//;s/^[^:]*: //')
+        
+        #echo "" >> "$mkvlog_output_file"
+        echo "> current_name: " >> "$mkvlog_output_file"
+        echo "$current_name" >> "$mkvlog_output_file"
+        
+        # TODO - Regex implimentation for searching the word to remove in one go
+        if [[ "$current_name" == *"$word_to_remove"* ]]; then
+            new_name=$(echo "$current_name" | sed "s/$word_to_remove//g")
+            
+            echo "> word: $word_to_remove found in track: $track_number" >> "$mkvlog_output_file"
+            echo "> new_name: " >> "$mkvlog_output_file"
+            echo "$new_name"
+            
+            # Log mediainfo & mkvinfo only once and only if there is a word changes
+            if [ "$mkv_log" = true ]; then
+                echo "" >> "$mkvlog_output_file"
+                echo "-----------------------------------------------------------------" >> "$mkvlog_output_file"
+                echo "> Media Info START | --------------------------------------------" >> "$mkvlog_output_file"
+                mediainfo "$filename" >> "$mkvlog_output_file"
+                echo "> Media Info END | ----------------------------------------------" >> "$mkvlog_output_file"
+                echo "" >> "$mkvlog_output_file"
+                echo "> MKV Info START | ----------------------------------------------" >> "$mkvlog_output_file"
+                mkvinfo "$filename" >> "$mkvlog_output_file"
+                echo "> MKV Info END | ------------------------------------------------" >> "$mkvlog_output_file"
+                echo "-----------------------------------------------------------------" >> "$mkvlog_output_file"
+                
+                # ntfy notification on matched names
+                # Extract the filename using basename
+                base_mkvfilename=$(basename "$filename")
+                curl -d "MKVRN API: Found $word_to_remove in $base_mkvfilename" "ntfy.sh/dragondb_ntfy"
+                
+                declare -g mkv_log=false
+            fi
+            
+            #echo "" >> "$mkvlog_output_file"
+            echo "Executing mkv delete name process on Track: $track_number" >> "$mkvlog_output_file"
+            mkvpropedit "$filename" --edit track:$track_number --delete name
+            
+            #echo "" >> "$mkvlog_output_file"
+            echo "Word '$word_to_remove' removed from Track $track_number name metadata."
+            echo "Word '$word_to_remove' removed from Track $track_number name metadata." >> "$mkvlog_output_file"
+            echo "_________________________________________________________________" >> "$mkvlog_output_file"
+        else
+            #echo "" >> "$mkvlog_output_file"
+            echo "Word '$word_to_remove' not found in Track $track_number name metadata."
+            echo "Word '$word_to_remove' not found in Track $track_number name metadata. Skipping." >> "$mkvlog_output_file"
+            echo "_________________________________________________________________" >> "$mkvlog_output_file"
+        fi
+    done
+    
+    return
+}
+
+# Check if exactly one argument (the MKV file path) is provided
+if [ "$#" -ne 1 ]; then
+    echo "Invalid number of arguments provided"
+    echo "Invalid Argments, Usage: $0 <path_to_mkv_file>" >> "$mkvlog_output_file"
+    exit 1
+fi
+
+# Get the MKV file path from the argument
+full_mkv_path="$1"
+
+# Echo the MKV file path
+echo "Received MKV file path: $full_mkv_path "
+echo "#################################################################" >> "$mkvlog_output_file"
+echo "Received MKV file path: $full_mkv_path" >> "$mkvlog_output_file"
+echo "" >> "$mkvlog_output_file"
+
+# Check if the provided file is a .mkv file & it exists
+if [[ "$full_mkv_path" != *.mkv ]] || [ ! -f "$full_mkv_path" ]; then
+    echo "Error: The provided file is not a .mkv file or does not exist."
+    echo "Error: The provided file is not a .mkv file or does not exist." >> "$mkvlog_output_file"
+    exit 1
+fi
+
+# Define an array of word to remove and pass it as arguments to mkv_rename_func
+words_to_remove_arr=("TamilMV" "TamilCV" "GalaxyRG" "MovieRulz" "TamilBlasters" "Full4Movies")
+
+# Iterate over the array and call mkv_rename function
+for word in "${words_to_remove_arr[@]}"; do
+    mkv_rename_func "$full_mkv_path" "$word"
+done
+
+echo "MKVRename API Script executed successfully"
+echo "=================================================================" >> "$mkvlog_output_file"
+echo "" >> "$mkvlog_output_file"
+echo "" >> "$mkvlog_output_file"
